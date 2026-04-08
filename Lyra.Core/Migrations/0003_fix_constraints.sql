31a4bbcd913e0aa07ad1b79a1d931f1cb3d22256
Lukas Höppner <lukas.hoeppner@gmail.com>
2026-04-08 13:47:16 +0200

    combine migrations

--- Lyra.Core/Migrations/0003_fix_constraints.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE external_connection_account
-    ADD CONSTRAINT uq_connection_external_account UNIQUE (connection_id, external_account_id);
-
-ALTER TABLE external_connections
-    ADD COLUMN IF NOT EXISTS available_balance_types jsonb NULL;
-
-ALTER TABLE lyra.transactions
-    ADD COLUMN IF NOT EXISTS currency VARCHAR(10) NOT NULL DEFAULT '';
